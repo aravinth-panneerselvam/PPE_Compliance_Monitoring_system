760d34593273318ca82662a8a59a9b51e990d4e0
aravinth-panneerselvam <aravinth.pta@gmail.com>
2025-12-07 18:00:14 +0530

    src

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from "./components/Login";
+import { Home } from './components/home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
